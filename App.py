1aba175f7a4e63e2fc4db91c2c968b84abaf349a
Everlynk <suexte@gmail.com>
2025-04-11 17:01:43 +0200

    Update App.py

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Leafcura",
     page_icon="leafcurafix_favicon_512x512.png",
     layout="centered")
 
-logo = Image.open("leafcura_logo_.png")
+logo = Image.open("leafcura_logo.png")
 st.image(logo, width=200)
 
 # Sprachwahl
